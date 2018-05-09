642ebddf109f27ff3058ab7a9126de7f78310d0b
ajosh8 <amol.joshi@nike.com>
2018-05-09 00:57:10 -0700

    chore(emotion): remove css imports

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,8 +1,8 @@
 import React from "react";
-import styled, { css } from "react-emotion";
+import styled from "react-emotion";
 
 const BodyStyle = styled("div")`
-  background: ${({ theme: { colors }}) => colors.grey.superLight};
+  background: ${({ theme: { colors } }) => colors.grey.superLight};
   width: 100%;
   height: 90%;
   text-align: center;
